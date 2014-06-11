c9f4008e9c57655a19245da44a4f3cc175450c51
Brian Hanson <brian@brianhanson.net>
2014-06-11 13:18:46 -0500

    adds ability to set min, max or regular old height

--- src/jquery.equalizer.js
+++ src/jquery.equalizer.js
@@ -3,6 +3,7 @@
   $.fn.equalizer = function(options) {
 
     var settings = $.extend( {
+      heightProperty: 'height'      // gets passed straight through so accepts min-height, max-height or height
     }, options );
 
     var maxHeight = 0;
@@ -21,8 +22,16 @@
     });
 
     // Returning
-    return this.outerHeight(maxHeight);
-    
+    if(settings.heightProperty === 'min') {
+      this.css({ 'min-height' : maxHeight});
+    } else if(settings.heightProperty === 'max') {
+      this.css({ 'max-height' : maxHeight});
+    } else {
+      this.css({ 'height' : maxHeight});
+    }
+
+    // Allows this to be chained
+    return this;
     
   };
 
